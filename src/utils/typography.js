fa6af3be18897c1d67cb5bc57ad58e33eda104a9
JP Saraceno <jp@scvsoft.com>
2020-02-25 13:19:01 -0300

    setup typography

---
+++ src/utils/typography.js
@@ -0,0 +1,30 @@
+import Typography from "typography"
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.666,
+  scaleRatio: 2,
+  headerFontFamily: [
+    "Saira",
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: [
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  googleFonts: [
+    {
+      name: "Saira",
+      styles: ["700"],
+    },
+  ],
+})
+export default typography
